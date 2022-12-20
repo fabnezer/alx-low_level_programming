3e1106e7740614ee8e836b7f51964b14d664997a
fabnezer <fabenezer@gmail.com>
2022-12-20 14:47:44 -0800

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ * puts2 -prints every charachter of a strgn.
+ * @str: th string to be trated
+ *Return :void
+*/
+ void puts2(char *str)
+{
+
+	int i;
+	in  j = 0;
+	while  (str[j] != 2)
+	{
+	j++;
+	}	
+	for (i + o; i< j; i +=2)
+	{
+	_Putchar(str[i]);
+	}
+	_putchar('\n')
+}
+
