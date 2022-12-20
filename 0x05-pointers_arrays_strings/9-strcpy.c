fe329c0e9599c85fd0c739cbb33fb4ee1631a357
fabnezer <fabenezer@gmail.com>
2022-12-20 15:46:09 -0800

    tsk 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * _strcpy - copy a sstring
+ * @dest: destination of valu
+ * @src: source value
+ * Return: the popular to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	dest[i] = src[i];
+	}
+	dest[i++] = src[i];
+	return (dest);
+
+}
