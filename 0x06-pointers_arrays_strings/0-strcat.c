cdb20d498055be93e045ff0ca111bcdaac2ae923
fabnezer <fabenezer@gmail.com>
2022-12-21 11:12:59 -0800

    more pointes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,19 +8,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	
 	char *dest_end = dest;
 	    while (*dest_end != '\0')
 	   {
 	    dest_end++;
 	   }
-	   while(*src != '\0')
+	   while (*src != '\0')
 	   {
 	      *dest_end = *src;
 	       dest_end++;
 	       src++;
 	    }
 	    *dest_end = '\0';
-	     return dest;
+	     return (dest);
 
 }
